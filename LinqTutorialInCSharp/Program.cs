df769950ce45be655bdb6f5ee546ebbb3b448a08
makand <makand@evenica.com>
2023-04-12 00:16:18 +0600

    OrderBy Added

--- LinqTutorialInCSharp/Program.cs
+++ LinqTutorialInCSharp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqTutorialInCSharp
 {
@@ -6,7 +8,22 @@ namespace LinqTutorialInCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var dataSourceInt = new List<int> { 1, 5, 2, 4, 9, 3 };
+            var dataSourceString = new List<String> { "Jibon", "Akash", "PrapTi", "BullShit", "Jamela", "Karim" };
+
+            var methodSyntaxForInt = dataSourceInt.OrderBy(x => x).ToList();
+
+            var methodSyntaxForString = dataSourceString.OrderBy(x => x).ToList();
+
+            var querySyntaxForInt = (from qs in dataSourceInt
+                                     orderby qs
+                                     select qs).ToList();
+
+            var querySyntaxForString = (from qs in dataSourceString
+                                     orderby qs
+                                     select qs).ToList();
+
+            Console.ReadLine();
         }
     }
 }
