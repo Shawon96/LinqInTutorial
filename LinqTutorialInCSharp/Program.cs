603763588aa169bd1078274fa9164ab95f80014f
makand <makand@evenica.com>
2023-04-11 23:29:48 +0600

    Update OffType

--- LinqTutorialInCSharp/Program.cs
+++ LinqTutorialInCSharp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqTutorialInCSharp
 {
@@ -6,7 +8,19 @@ namespace LinqTutorialInCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var dataSource = new List<Object> { "Akash", "Shuvo", "Nafi", 1, 2, 3, 4, 5 };
+
+            var methodSyntax = dataSource.OfType<int>().ToList();
+            var methodSyntax2 = dataSource.OfType<string>().ToList();
+
+            var querySyntax = (from source in dataSource
+                               where source is int
+                               select source).ToList();
+            var querySyntax2 = (from source in dataSource
+                               where source is String
+                               select source).ToList();
+
+            Console.ReadLine();
         }
     }
 }
